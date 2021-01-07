5016c907c0c75aa4086e3c0de6892d29ba767ea7
Anshu Raj <anshusopinion@gmail.com>
2021-01-07 09:43:46 +0530

    card dropdown made

--- src/components/WorkCard.tsx
+++ src/components/WorkCard.tsx
@@ -2,23 +2,55 @@ import { Box, Flex, Heading, Progress, Stack } from "@chakra-ui/react";
 import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Card } from "elements";
-import React from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import React, { useState } from "react";
 
 const WorkCard: React.FC = () => {
+  const [isCardOpen, setIsCardOpen] = useState(false);
+  console.log(isCardOpen);
+
+  const toggle = () => {
+    setIsCardOpen((prev) => !prev);
+  };
+
   return (
     <>
       <Card>
-        <Flex justifyContent="space-between">
-          <Box width={7 / 8}>
-            <Stack>
-              <Heading as="h6" fontSize={{ base: "md", md: "xl" }}>
-                Learing Figma
-              </Heading>
-              <Progress colorScheme="red" value={50} />
-            </Stack>
-          </Box>
-          <FontAwesomeIcon icon={faAngleDown} />
-        </Flex>
+        <Stack>
+          <Flex justifyContent="space-between" onClick={toggle}>
+            <Box width={7 / 8}>
+              <Stack as={motion.div}>
+                <Heading as="h6" fontSize={{ base: "md", md: "xl" }}>
+                  Learing Figma
+                </Heading>
+                <Progress colorScheme="red" value={50} />
+              </Stack>
+            </Box>
+            <Box as={motion.div} whileTap={{ scale: 1.4 }}>
+              <FontAwesomeIcon icon={faAngleDown} size="2x" />
+            </Box>
+          </Flex>
+
+          <Flex>
+            <AnimatePresence>
+              {isCardOpen && (
+                <Box
+                  as={motion.div}
+                  initial={{ height: 0 }}
+                  animate={{ height: "auto" }}
+                  exit={{
+                    height: 0,
+                    transition: {
+                      duration: 0.3,
+                    },
+                  }}
+                >
+                  hi
+                </Box>
+              )}
+            </AnimatePresence>
+          </Flex>
+        </Stack>
       </Card>
     </>
   );
