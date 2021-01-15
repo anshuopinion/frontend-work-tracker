0ac780874ba1dbe6b5d1ac41f19129bf48dc5616
Anshu Raj <anshusopinion@gmail.com>
2021-01-15 16:07:12 +0530

    animate added in cards

--- src/components/ui/FormCard.tsx
+++ src/components/ui/FormCard.tsx
@@ -1,5 +1,6 @@
 import { Flex } from "@chakra-ui/react";
 import { Card } from "elements";
+import { motion } from "framer-motion";
 import { FC } from "react";
 
 const LSCard: FC = ({ children }) => {
@@ -10,6 +11,20 @@ const LSCard: FC = ({ children }) => {
       color="mainD"
       maxWidth="30rem"
       my="2rem"
+      as={motion.div}
+      initial="hidden"
+      animate="show"
+      variants={{
+        hidden: {
+          y: "-100vh",
+          scale: 0,
+        },
+        show: {
+          y: 0,
+          scale: 1,
+          transition: { duration: 0.5 },
+        },
+      }}
     >
       <Card
         rounded={{ base: "none", md: "lg" }}
